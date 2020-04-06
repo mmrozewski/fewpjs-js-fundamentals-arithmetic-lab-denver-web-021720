e01f0627198e8b307f8d0fd64e80284146cbefb8
mmrozewski <mmrozews@gmail.com>
2020-04-06 17:51:28 -0500

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+
+let multiply = num1 * num2;
+
+let random = parseInt(Math.random() * 10);
+
+let mod = 19 % 5;
+
+let max = Math.max(2, 2, 4, 6, 20, 1, 17);
